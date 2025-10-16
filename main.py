76017a89d3671f74fa7f177501baac76a7901f73
jess <jessachandler@gmail.com>
2025-10-16 15:11:50 -0700

    allow pass through of model size

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="text_from_video.")
     parser.add_argument("video_path", type=str, help="Full path to video file")
     parser.add_argument("--text_path", type=str, default="video_transcript.txt", help="Full path to save text")
+    parser.add_argument("--model", type=str, default="tiny", help="whisper model size")
 
     args = parser.parse_args()
-    tfv.get_transcript_save_to_text(args.video_path, args.text_path)
+    tfv.get_transcript_save_to_text(args.video_path, args.text_path, args.model)
--- src/text_from_video/text_from_video.py
+++ src/text_from_video/text_from_video.py
@@ -21,7 +21,7 @@ def save_text_to_path(fpath, ftext):
     with open(fpath, "w") as file:
       file.write(ftext)
 
-def get_transcript_save_to_text(the_video_path, the_text_path):
-    the_result = get_transcript_from_video(the_video_path, model_select="small")
+def get_transcript_save_to_text(the_video_path, the_text_path, the_model):
+    the_result = get_transcript_from_video(the_video_path, model_select=the_model)
     transcribed_text = the_result["text"]
     save_text_to_path(fpath = the_text_path, ftext = transcribed_text)
